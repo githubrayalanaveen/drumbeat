d70cbd0581379642a1747f33ae8e8b43790f55bf
githubrayalanaveen <38714499+githubrayalanaveen@users.noreply.github.com>
2020-03-09 22:50:38 +0530

    drumbeat

---
+++ index.js
@@ -0,0 +1,77 @@
+//Instead of calling function() directly we want the click event to happen for function to work.
+// document.querySelector("button").addEventListener("click", handleClick);
+//
+// function handleClick() {
+//   alert("I got clicked");
+// }
+//The other way is this.
+for (i=0; i<document.querySelectorAll(".drum").length; i++) {
+
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+
+    document.addEventListener("keypress", function(event) {
+      makeSound(event.key);
+      buttonAnimation(event.key);
+});
+
+  });
+
+}
+
+
+function makeSound(key) {
+
+  switch (key) {
+
+    case "w":
+    var audio = new Audio('tom-1.mp3');
+                audio.play();
+                break;
+
+    case "a":
+    var audio = new Audio('tom-2.mp3');
+                audio.play();
+                break;
+
+    case "s":
+                var audio = new Audio('tom-3.mp3');
+                audio.play();
+                break;
+
+    case "d":
+                var audio = new Audio('tom-4.mp3');
+                audio.play();
+                break;
+
+    case "j":
+                var audio = new Audio('crash.mp3');
+                audio.play();
+                break;
+
+    case "k":
+                var audio = new Audio('crash.mp3');
+                audio.play();
+                break;
+
+    case "l":
+                var audio = new Audio('crash.mp3');
+                audio.play();
+                break;
+
+    default: console.log(buttonInnerHTML);
+
+
+  }
+}
+
+function buttonAnimation(currentKey) {
+var activeButton = document.querySelector("." + currentKey);
+activeButton.classList.add("pressed");
+setTimeout(function(){
+  activeButton.classList.remove("pressed");
+}, 100);
+}
